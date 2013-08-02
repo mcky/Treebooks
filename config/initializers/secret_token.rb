90551f4ae07030f61fc2761f47a72aea3d215f75
Ross Mackay <ross@mcky.me>
2013-08-02 19:27:12 +0100

    Created a rails application and added satus updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebooks::Application.config.secret_token = '11c043f3822bd2d6db369b476df031c37b842e7ca065d9c1dbdaa749687c89ba3ee0070138a6da4c75d120b9ac0dcbee0895b7fe2b31f2a2e3dba30f9bb1d21b'
